75c4e3d11e3abd333fbee28922d853eac3ed86d6
Tiny Giant <ajsa.ferguson@gmail.com>
2015-09-25 19:39:52 -0600

    Update CVRequestArchiver.user.js

--- CVRequestArchiver.user.js
+++ CVRequestArchiver.user.js
@@ -28,26 +28,28 @@
                 }
             }
             var num = requests.length - 1;
-            var postchk = setInterval(function(){
-                if(num === 0) clearInterval(postchk);
-                GM_xmlhttpRequest({
-                    method: 'GET',
-                    url: 'http://stackoverflow.com/flags/questions/' + requests[num].post + '/close/popup',
-                    onload: function(resp){
-                        requests[num].closed = /This question is now closed/.test(resp.response);
-                        console.log(requests[num]);
-                        if(num-- === 0) {
-                            var ids = [];
-                            for(var i in requests) if(requests[i].closed) ids.push(requests[i].msg);
-                            $.ajax({
-                                data: 'ids=' + ids.join('%2C') + '&to=90230&fkey=' + $('#fkey').val(),
-                                type: 'POST',
-                                url: '/admin/movePosts/41570'
-                            });
+            if(num >= 0) {
+                var postchk = setInterval(function(){
+                    if(num === 0) clearInterval(postchk);
+                    GM_xmlhttpRequest({
+                        method: 'GET',
+                        url: 'http://stackoverflow.com/flags/questions/' + requests[num].post + '/close/popup',
+                        onload: function(resp){
+                            requests[num].closed = /This question is now closed/.test(resp.response);
+                            console.log(requests[num]);
+                            if(num-- === 0) {
+                                var ids = [];
+                                for(var i in requests) if(requests[i].closed) ids.push(requests[i].msg);
+                                $.ajax({
+                                    data: 'ids=' + ids.join('%2C') + '&to=90230&fkey=' + $('#fkey').val(),
+                                    type: 'POST',
+                                    url: '/admin/movePosts/41570'
+                                });
+                            }
                         }
-                    }
-                });
-            }, 4000);
+                    });
+                }, 4000);
+            }
         }
     });
 })();
