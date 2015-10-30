b61e6237bd86eb99a62d1bcceba5b7032bc304c4
Tiny Giant <ajsa.ferguson@gmail.com>
2015-10-30 10:40:48 -0600

    Update SOZuluTime.user.js

--- SOZuluTime.user.js
+++ SOZuluTime.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         SO Zulu Time
 // @namespace    http://github.com/Tiny-Giant
-// @version      1.0.0.0
+// @version      1.0.0.1
 // @description  Make all relative times zulu
 // @author       @TinyGiant
 // @include      /^https?://\w*.?(stack(exchange|overflow)|serverfault|superuser|askubuntu|stackapps|mathoverflow)\.(com|net).*/
 // @grant        none
 // ==/UserScript==
 
+$('.user-action-time, .user-action-time a').each(function(){ this.firstChild.remove() })
 $('[class*="relativetime"]').each(function(){ this.textContent = this.title });
