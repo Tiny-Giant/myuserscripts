80e5bb8fbad0ff17f70bb4c68ccf7377c81db8ac
Tiny Giant <ajsa.ferguson@gmail.com>
2016-01-31 21:42:33 -0800

    Create ReviewEnhancements.user.js

---
+++ ReviewEnhancements.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         Review Enhancements
+// @namespace    http://github.com/Tiny-Giant
+// @version      1.0.0.0
+// @description  The review queues are abstractions with some limitations. This script removes those limitations on questions
+// @author       @TinyGiant
+// @include      /https?:\/\/(meta\.|www\.)?stackoverflow.com\/review\/(?!custom).*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+let initQuestion = (post) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', event => {
+        if(xhr.status !== 200) {
+            console.log(xhr.status, xhr.statusText, xhr.responseText);
+            return;
+        }
+        
+        StackExchange.question.init({
+            votesCast: JSON.parse(xhr.responseText),
+            canViewVoteCounts: true,
+            questionId: post
+        });
+        
+        let commentLinks = document.querySelectorAll('.js-show-link.comments-link');
+        
+        for(let i in Object.keys(commentLinks)) commentLinks[i].click();
+    }, false);
+
+    xhr.open('GET', '/posts/' + post.dataset.questionid + '/votes');
+
+    xhr.send();
+}
+
+let getPost = (post) => {
+    if (!post) return;
+    
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', event => {
+        if(xhr.status !== 200) {
+            console.log(xhr.status, xhr.statusText, xhr.responseText);
+            return;
+        }
+        
+        post.innerHTML = xhr.responseText;
+        
+        if (/question/.test(post.className)) initQuestion(post);
+    }, false);
+
+    xhr.open('GET', '/posts/ajax-load-realtime/' + (post.dataset.questionid || post.dataset.answerid));
+
+    xhr.send();
+}
+
+$(document).ajaxComplete((one, two, three) => { 
+    if(!/next\-task|task\-reviewed/.test(three.url)) return;
+    
+    let answers  = document.querySelectorAll('.answer');
+    
+    for(let i in Object.keys(answers)) getPost(answers[i]);
+    
+    getPost(document.querySelector('.question'));
+});
