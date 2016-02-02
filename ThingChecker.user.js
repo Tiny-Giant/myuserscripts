c367410080528f0ae84b72ac7b75d6c563b95543
Tiny Giant <ajsa.ferguson@gmail.com>
2016-02-02 12:58:34 -0800

    updated

--- ThingChecker.user.js
+++ ThingChecker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name           Review Audit Finder
+// @name           Thing Checker
 // @namespace      https://github.com/Tiny-Giant/
-// @version        1.0.0.4
+// @version        1.0.0.5
 // @description    Finds real post scores in the review queues and puts it under the assumed score, also signals if it is an audit.
 // @author         @TinyGiant
 // @include        /^https?://\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com/review/.*/
@@ -15,19 +15,24 @@
  *********************************************************/
 
 (function(){
-    var headerWidth = $('#header').width();
-    var logoWidth = $('#hlogo').width();
-    var navWidth = $('#hmenus').width();
-    var availableWidth = headerWidth - (logoWidth + navWidth);
+    let headerWidth = $('#header').width();
+    let logoWidth = $('#hlogo').width();
+    let navWidth = $('#hmenus').width();
+    let availableWidth = headerWidth - (logoWidth + navWidth);
 
-    var auditHolder = $('<div/>').css({'position':'absolute','left':logoWidth + 'px','font-size':'2em','font-weight':'bold','line-height':'75px','width':availableWidth + 'px','text-align':'center'});
+    let auditHolder = $('<div/>').css({'position':'absolute','left':logoWidth + 'px','font-size':'2em','font-weight':'bold','line-height':'75px','width':availableWidth + 'px','text-align':'center'});
     $('#hmenus').before(auditHolder);
 
     $(document).ajaxComplete(function() { 
         if(/review.next\-task|review.task\-reviewed/.test(arguments[2].url)) {
-            var postData = JSON.parse(arguments[1].responseText);
-            if(!postData.postTypeId) return;
-            auditHolder.html(postData.isAudit ? '<span style="color:maroon">This is an audit!</span>' : '<span style="color:green">This is not an audit.</span>');
+            let postData = JSON.parse(arguments[1].responseText);
+            
+            if (!postData.postTypeId) return;
+            
+            let isAudit = postData.isAudit;
+            
+            auditHolder.html(['<span style="color:green">This is not an audit.</span>','<span style="color:maroon">This is an audit!</span>'][+isAudit]);
+            
             $.ajax({
                 type: 'GET',
                 url: '/posts/' + postData.postId + '/vote-counts',
