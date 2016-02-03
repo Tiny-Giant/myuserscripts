7bbc12aded7b4f480663172df1ace7f7fbcedfba
Tiny Giant <ajsa.ferguson@gmail.com>
2016-02-03 15:20:51 -0800

    Update AJAXListener.user.js

--- AJAXListener.user.js
+++ AJAXListener.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         AJAX Listener
 // @namespace    https://github.com/Tiny-Giant/
-// @version      1.0.0.0
+// @version      1.0.0.2
 // @description  This logs the results of any AJAX requests made to the console. 
 // @author       @TinyGiant
 // @include      /https?:\/\/\w*.?stackoverflow.com/.*/
 // @grant        none
 // ==/UserScript==
+/* jshint -W097 */
+'use strict';
 
-$(document).ajaxComplete(function(){
-    console.log(arguments);
+$(document).ajaxComplete((event, request, settings) => {
+    console.log(event, request, settings);
+    console.log('\n');
 });
