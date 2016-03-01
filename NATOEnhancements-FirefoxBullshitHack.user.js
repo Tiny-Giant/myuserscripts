2872aee1b3241594e3067faa11b5baa44ab66aea
Tiny Giant <ajsa.ferguson@gmail.com>
2016-02-29 19:19:59 -0800

    Update NATOEnhancements-FirefoxBullshitHack.user.js

--- NATOEnhancements-FirefoxBullshitHack.user.js
+++ NATOEnhancements-FirefoxBullshitHack.user.js
@@ -1,42 +1,23 @@
 // ==UserScript==
 // @name         NATO Enhancements - FirefoxBullshitHack
 // @namespace    http://github.com/Tiny-Giant
-// @version      1.0.0.1
+// @version      1.0.0.4
 // @description  Includes the actual post on the new answers to old questions page of the 10k tools. 
 // @author       @TinyGiant
 // @include      /https?:\/\/(meta\.)?stackoverflow.com\/tools\/new-answers-old-questions.*/
-// @grant        GM_addStyle
+// @grant        none
 // ==/UserScript==
 /* jshint -W097 */
-/* jshint esversion: 6 */
+/* jshint esnext: true */
 'use strict';
 
 let FirefoxBullshitHack = function(){
     'use strict';
-    let css = [
-        '.question, .answer {',
-        '    width: 730px !important',
-        '}',
-        'h1, h2, h3, h4, h5, h6 {',
-        '    font-weight: normal',
-        '}',
-        '.answer {',
-        '    background: RGBA(255,153,0, 0.1);',
-        '}'
-    ].join('\n');
-    document.body.appendChild(document.createElement("style").appendChild(document.createTextNode(css)).parentNode);
-    
-    StackExchange.helpers.addSpinner(document.querySelector('.subheader h1'));
-    
-    StackExchange.using("inlineEditing", function () {
-        StackExchange.inlineEditing.init();
-    });
     
     const funcs = {};
     
     funcs.replacePost = (post, html) =>
     {
-    
         post.wrap.innerHTML = html;
     
         const osnippets = post.snippets;
@@ -120,7 +101,6 @@ let FirefoxBullshitHack = function(){
             promises.push(funcs.fetchPost(post.answer, html => 
             {
                 funcs.replacePost(post.answer, html);
-                post.answer.wrap.className = '';
             }));
             
             // Get votes
@@ -144,6 +124,16 @@ let FirefoxBullshitHack = function(){
         {
             console.log(xhr);
             
+            funcs.addCSS();
+            
+            for(let post of posts)
+            {
+                post.nodes.columns[0].innerHTML = '';
+                post.nodes.columns[1].innerHTML = '';
+                post.nodes.columns[0].appendChild(post.nodes.wrap);
+                post.nodes.wrap.style.display = '';
+            }
+            
             StackExchange.question.init({
                 votesCast: postvotes,
                 canViewVoteCounts: true,
@@ -160,37 +150,63 @@ let FirefoxBullshitHack = function(){
             
             for(let post of posts)
             {
-                StackExchange.realtime.subscribeToQuestion('1', post.question.id);
+                StackExchange.realtime.subscribeToQuestion(StackExchange.options.site.id, post.question.id);
             }
             
             StackExchange.helpers.removeSpinner(document.querySelector('.subheader h1'));
-        },xhr =>
+        }, xhr =>
         {
             console.log('Failed', xhr);
         });
     };
     
+    funcs.appendNodes = posts =>
+    {
+        for(let post of posts)
+        {
+            post.nodes.columns[0].appendChild(post.nodes.wrap);
+        }
+    };
+    
     funcs.getPosts = () =>
     {
         const posts = [];
-        const answers = Array.from(document.querySelectorAll('.post-text'));
+        const rows = Array.from(document.querySelectorAll('.default-view-post-table > tbody > tr'));
     
-        for(let answer of answers)
+        for(let row of rows)
         {
             const nodes = {};
             
-            nodes.answer = answer;
-            nodes.question = document.createElement('div');
-            nodes.snippets = Array.from(nodes.answer.querySelectorAll('.snippet'));
+            nodes.scope = row;
+            nodes.columns = Array.from(nodes.scope.children);
+            nodes.wrap = document.createElement('div');
+            nodes.wrap.style.display = 'none';
             
-            nodes.parent = answer.parentNode;
-            nodes.details = nodes.parent.nextElementSibling;
-            nodes.title = nodes.parent.querySelector('.answer-hyperlink');
+            nodes.old = {};
             
-            const urlsections = /(\d+)\/(.*?)\/(\d+)/.exec(nodes.title.href);
+            nodes.old.answer = nodes.scope.querySelector('.post-text');
+            nodes.old.title = nodes.scope.querySelector('.answer-hyperlink');
+            
+            const urlsections = /(\d+)\/(.*?)\/(\d+)/.exec(nodes.old.title.href);
             const questionid = urlsections[1];
             const titleslug = urlsections[2];
             const answerid = urlsections[3];
+            
+            nodes.title = document.createElement('h1');
+            nodes.wrap.appendChild(nodes.title);
+            
+            nodes.link = document.createElement('a');
+            nodes.link.href = window.location.protocol + '//' + window.location.host + '/questions/' + questionid + '/' + titleslug;
+            nodes.link.textContent = nodes.old.title.textContent;
+            nodes.title.appendChild(nodes.link);
+            
+            nodes.question = document.createElement('div');
+            nodes.wrap.appendChild(nodes.question);
+            
+            nodes.answer = document.createElement('div');
+            nodes.wrap.appendChild(nodes.answer);
+            
+            nodes.snippets = Array.from(nodes.old.answer.querySelectorAll('.snippet'));
             
             posts.push({
                 answer: {
@@ -202,25 +218,55 @@ let FirefoxBullshitHack = function(){
                     id:   questionid,
                     wrap: nodes.question,
                     snippets: []
-                }
+                },
+                nodes: nodes
             });
-            
-            nodes.title.href = window.location.protocol + '//' + window.location.host + '/questions/' + questionid + '/' + titleslug;
-            nodes.title.className = '';
-            nodes.parent.insertBefore(nodes.question, nodes.answer);
-            nodes.parent.insertBefore(document.createElement('br'), nodes.answer);
-            nodes.title.parentNode.removeChild(nodes.title.nextElementSibling);
-            nodes.title.parentNode.removeChild(nodes.title.nextElementSibling);
-            nodes.parent.parentNode.removeChild(nodes.details);
-            nodes.title.outerHTML = '<h1>' + nodes.title.outerHTML + '</h1>';
         }
         
         return posts;
     };
     
-    const posts = funcs.getPosts();
+    funcs.addCSS = () =>
+    {
+        const CSS = [
+            '.question, .answer {',
+            '    width: 730px !important',
+            '}',
+            'h1, h2, h3, h4, h5, h6 {',
+            '    font-weight: normal',
+            '}',
+            '.answer {',
+            '    background: RGBA(255,153,0, 0.1);',
+            '    margin-top: 1em;',
+            '}'
+        ].join('\n');
     
-    funcs.fetchAllPosts(posts);
+        const style = document.createElement('style');
+        document.body.appendChild(style);
+        
+        const text = document.createTextNode(CSS);
+        style.appendChild(text);
+    }
+    
+    funcs.init = () =>
+    {
+        const posts = funcs.getPosts();
+        
+        const title = document.querySelector('.subheader h1');
+        
+        StackExchange.helpers.addSpinner(title);
+    
+        StackExchange.using("inlineEditing", () => 
+        {
+            StackExchange.inlineEditing.init();
+        });
+        
+        funcs.appendNodes(posts);
+    
+        funcs.fetchAllPosts(posts);
+    };
+    
+    funcs.init();
 };
 
 let FirefoxBullshitHackScript = document.createElement('script');
