f15a653b88f320d2080737ae4a288dd3be6e17de
Tiny Giant <ajsa.ferguson@gmail.com>
2017-03-13 13:56:45 -0700

    Update ThingChecker.user.js

--- ThingChecker.user.js
+++ ThingChecker.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name           Thing Checker
 // @namespace      https://github.com/Tiny-Giant/
-// @version        1.0.0.5
-// @description    Finds real post scores in the review queues and puts it under the assumed score, also signals if it is an audit.
+// @version        1.0.0.6
+// @description    Indicates if the current review task is an audit; shows true post score for audits.
 // @author         @TinyGiant
 // @include        /^https?://\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com/review/.*/
 // @grant          none
 // ==/UserScript==
 /* jshint -W097 */
+/* jshint: esnext: true */
 'use strict';
 
 /*********************************************************
@@ -16,35 +17,72 @@
  * If you get caught using this you may be review banned *
  *********************************************************/
 
-(function(){
-    let headerWidth = $('#header').width();
-    let logoWidth = $('#hlogo').width();
-    let navWidth = $('#hmenus').width();
-    let availableWidth = headerWidth - (logoWidth + navWidth);
+const ThingChecker = (function() { 
+    document.querySelector('.review-actions').insertAdjacentHTML('beforebegin', `
+        <h1 class="audit-indicator"></h1>
+        <span class="lsep">|</span>
+        <span class="indicator-counts"></span>
+        <style type="text/css">
+            .audit-indicator {
+                display: inline-block;
+                margin: 0;
+                padding: 0;
+                line-height: 1.0em;
+                vertical-align: middle;
+            }
+            .audit-indicator {
+                color: red;
+            }
+            .audit-indicator > .indicator-counts {
+                font-size: 0.8em;
+                display: inline-block;
+                vertical-align: middle;
+            }
+        </style>
+    `);
+    
+    let indicator = document.querySelector('.audit-indicator');
+    let counts = document.querySelector('.indicator-counts');
+    
+    const posts = (function() {
+        const funcs = {
+            votes: post => new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
 
-    let auditHolder = $('<div/>').css({'position':'absolute','left':logoWidth + 'px','font-size':'2em','font-weight':'bold','line-height':'75px','width':availableWidth + 'px','text-align':'center'});
-    $('#hmenus').before(auditHolder);
+                xhr.addEventListener('load', () => {
+                    if(xhr.status !== 200)
+                        return reject(xhr);
 
-    $(document).ajaxComplete(function() { 
-        if(/review.next\-task|review.task\-reviewed/.test(arguments[2].url)) {
-            let postData = JSON.parse(arguments[1].responseText);
-            
-            if (!postData.postTypeId) return;
-            
-            let isAudit = postData.isAudit;
-            
-            auditHolder.html(['<span style="color:green">This is not an audit.</span>','<span style="color:maroon">This is an audit!</span>'][+isAudit]);
-            
-            $.ajax({
-                type: 'GET',
-                url: '/posts/' + postData.postId + '/vote-counts',
-                dataType: "json",
-                success: function (json) {
-                    $('.vote').after('<div class="vote"><div style="color:green">' + json.up + '</div>' +
-                                     '<div class="vote-count-separator"></div>' +
-                                     '<div style="color:maroon">' + json.down + '</div></div>');
-                }
-            });
+                    resolve(JSON.parse(xhr.response));
+                }, false);
+
+                xhr.open('GET', `/posts/${ post }/vote-counts`);
+
+                xhr.send();
+            })
+        };
+
+        return new Proxy({}, {
+            get: (target, post) => new Proxy({}, { get: (target, key) => funcs[key](post) })
+        });
+    })();
+    
+    let listener; // JSHint doesn't understand async / await
+    /* jshint ignore:start */
+    listener = async event => {
+        const json = JSON.parse(event.target.response);
+        indicator.textContent = json.isAudit ? 'This is an audit' : '';
+        counts.textContent = json.isAudit ? (votes => `(${ votes.up } / ${ votes.down })`)(await posts[json.postId].votes) : '';
+    };
+    /* jshint ignore:end */
+        
+    const open = XMLHttpRequest.prototype.open;
+
+    XMLHttpRequest.prototype.open = function(...args) {
+        if(/review.next\-task|review.task\-reviewed/.test(args[1])) {
+            this.addEventListener('load', listener, false);
         }
-    });
+
+        open.apply(this, args);
+    };
 })();
