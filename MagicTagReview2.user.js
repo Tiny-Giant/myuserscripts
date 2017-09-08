df7dd58520d48847099bc5d829ef85cc1af5aa14
Tiny Giant <ajsa.ferguson@gmail.com>
2017-09-08 15:40:13 -0700

    upversioned

--- MagicTagReview2.user.js
+++ MagicTagReview2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Magic™ Tag Review 2
 // @namespace    http://github.com/Tiny-Giant
-// @version      1.0.1.0
+// @version      1.0.1.1
 // @description  Custom review queue for tag oriented reviewing with the ability to filter by close votes and delete votes
 // @author       @TinyGiant
 // @contributor  @Makyen
