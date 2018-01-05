2a1cb8bf03ba3720364bc5d05f52fb649092ccb9
Makyen <wp.makyen@gmail.com>
2018-01-05 00:46:32 -0800

    Storage performance improvements   Add compression (LZ-String) for question list and queue.   Store filter settings/options as a single JSON value in localStorage, not GM storage.   Clean-up/transfer old data storage to current.

--- MagicTagReview2.user.js
+++ MagicTagReview2.user.js
@@ -8,6 +8,8 @@
 // @include      /^https?:\/\/\w*.?stackoverflow\.com\/review*/
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_listValues
+// @grant        GM_deleteValue
 // @grant        GM_addValueChangeListener
 // @run-at       document-start
 // ==/UserScript==
@@ -172,20 +174,90 @@ const inPageAddXHRListener = listen => {
 });*/
 executeInPage(inPageAddXHRListener, true, 'magicTagReview-addXHRListener');
 
-/** @Proxy store - Wraps GM_(set/get)value with a prefix to prevent interference with other scripts */
+//LZ-String 1.4.4
+// Copyright (c) 2013 Pieroxy <pieroxy@pieroxy.net>
+// This work is free. You can redistribute it and/or modify it
+// under the terms of the WTFPL, Version 2
+// For more information see LICENSE.txt or http://www.wtfpl.net/
+//
+// For more information, the home page:
+// http://pieroxy.net/blog/pages/lz-string/testing.html
+//
+// LZ-based compression algorithm, version 1.4.4
+// From: https://github.com/pieroxy/lz-string/blob/master/libs/lz-string.min.js
+var LZString=function(){function o(o,r){if(!t[o]){t[o]={};for(var n=0;n<o.length;n++)t[o][o.charAt(n)]=n}return t[o][r]}var r=String.fromCharCode,n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",t={},i={compressToBase64:function(o){if(null==o)return"";var r=i._compress(o,6,function(o){return n.charAt(o)});switch(r.length%4){default:case 0:return r;case 1:return r+"===";case 2:return r+"==";case 3:return r+"="}},decompressFromBase64:function(r){return null==r?"":""==r?null:i._decompress(r.length,32,function(e){return o(n,r.charAt(e))})},compressToUTF16:function(o){return null==o?"":i._compress(o,15,function(o){return r(o+32)})+" "},decompressFromUTF16:function(o){return null==o?"":""==o?null:i._decompress(o.length,16384,function(r){return o.charCodeAt(r)-32})},compressToUint8Array:function(o){for(var r=i.compress(o),n=new Uint8Array(2*r.length),e=0,t=r.length;t>e;e++){var s=r.charCodeAt(e);n[2*e]=s>>>8,n[2*e+1]=s%256}return n},decompressFromUint8Array:function(o){if(null===o||void 0===o)return i.decompress(o);for(var n=new Array(o.length/2),e=0,t=n.length;t>e;e++)n[e]=256*o[2*e]+o[2*e+1];var s=[];return n.forEach(function(o){s.push(r(o))}),i.decompress(s.join(""))},compressToEncodedURIComponent:function(o){return null==o?"":i._compress(o,6,function(o){return e.charAt(o)})},decompressFromEncodedURIComponent:function(r){return null==r?"":""==r?null:(r=r.replace(/ /g,"+"),i._decompress(r.length,32,function(n){return o(e,r.charAt(n))}))},compress:function(o){return i._compress(o,16,function(o){return r(o)})},_compress:function(o,r,n){if(null==o)return"";var e,t,i,s={},p={},u="",c="",a="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<o.length;i+=1)if(u=o.charAt(i),Object.prototype.hasOwnProperty.call(s,u)||(s[u]=f++,p[u]=!0),c=a+u,Object.prototype.hasOwnProperty.call(s,c))a=c;else{if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++),s[c]=f++,a=String(u)}if(""!==a){if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==r-1){d.push(n(m));break}v++}return d.join("")},decompress:function(o){return null==o?"":""==o?null:i._decompress(o.length,32768,function(r){return o.charCodeAt(r)})},_decompress:function(o,n,e){var t,i,s,p,u,c,a,l,f=[],h=4,d=4,m=3,v="",w=[],A={val:e(0),position:n,index:1};for(i=0;3>i;i+=1)f[i]=i;for(p=0,c=Math.pow(2,2),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(t=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 2:return""}for(f[3]=l,s=l,w.push(l);;){if(A.index>o)return"";for(p=0,c=Math.pow(2,m),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(l=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 2:return w.join("")}if(0==h&&(h=Math.pow(2,m),m++),f[l])v=f[l];else{if(l!==d)return null;v=s+s.charAt(0)}w.push(v),f[d++]=s+v.charAt(0),h--,s=v,0==h&&(h=Math.pow(2,m),m++)}}};return i}();"function"==typeof define&&define.amd?define(function(){return LZString}):"undefined"!=typeof module&&null!=module&&(module.exports=LZString);
+
+
+/** @Proxy store - Wraps GM_(set/get)value */
 //Use an in-script cache for what's being stored to GM storage. A side effect is that updates in other tabs
 //  are NOT seen cross-tab once the value is first read from storage. This provides semi-independance across
 //  tabs.
 var storageCache = {};
+try {
+    storageCache = JSON.parse(localStorage['MagicTagReview-storeageCache']);
+} catch(e) {
+}
+var storageCacheSeparate = {};
+var storageCacheTimeout=0;
+const storageSeparateList = ['queue', 'question_list', 'current'];
 const store = new Proxy({}, {
-    get: (t, k) => (typeof storageCache[k] === 'undefined' ? storageCache[k] = GM_getValue(`MagicTagReview-${ k }`) : storageCache[k]),
+    get: (target, key) => {
+        if (storageSeparateList.indexOf(key) > -1) {
+            if(key === 'current') {
+                var current = +localStorage['MagicTagReview-' + key];
+                return current ? current : 0;
+            } else {
+                if(typeof storageCacheSeparate[key] === 'undefined') {
+                    const lzString = GM_getValue(`${key}-LZString`);
+                    const uncompressed = LZString.decompressFromBase64(lzString);
+                    storageCacheSeparate[key] = uncompressed;
+                }
+                return storageCacheSeparate[key];
+            }
+        } else {
+            return storageCache[key];
+        }
+    },
     //Immediately update the cache and GM storage.
-    //Saving the data to GM storage asynchronously doesn't solve the problem.
-    //Storing large amounts of data to GM storage in Chrome/Tampermonkey is expensive. It will freeze the tab and
+    //Saving the data to GM storage asynchronously doesn't solve the problem of long times to store data.
+    //Storing large amounts of data or multiple values to GM storage in Chrome/Tampermonkey is expensive. It will freeze the tab and
     //  all Tampermonkey UI tabs (i.e. ones in the background context) for several seconds (it may affect all Tampermonkey
     //  operations (at least in the background context, which will probably affect loading scripts in new pages).
-    //  This is not the case in Firefox/Tampermonkey or Firefox/Greasemonkey.
-    set: (t, k, v) => (storageCache[k] = v, GM_setValue(`MagicTagReview-${ k }`, storageCache[k]), true)
+    //  Thus, only the queue and question_list are stored in GM storage. Everything else is stored in localStorage.
+    set: (target, key, value) => {
+        if (storageSeparateList.indexOf(key) > -1) {
+            if(key === 'current') {
+                localStorage['MagicTagReview-' + key] = value;
+            } else {
+                storageCacheSeparate[key] = value;
+                const compressed = LZString.compressToBase64(value);
+                const uncompressed = LZString.decompressFromBase64(compressed);
+                GM_setValue(`${key}-LZString`, compressed);
+                const lzString = GM_getValue(`${key}-LZString`);
+                const uncompressed2 = LZString.decompressFromBase64(lzString);
+            }
+        } else {
+            storageCache[key] = value;
+            //Don't stall execution for storing the chache (this doesn't actually help Chrome/Tampermonkey).
+            clearTimeout(storageCacheTimeout);
+            storageCacheTimeout = setTimeout(_ => {
+                localStorage['MagicTagReview-storeageCache'] = JSON.stringify(storageCache);
+            }, 0);
+        }
+        return true;
+    }
+});
+
+//Clean-up from earlier released version of the script/Transfer data from original GM_setValue keys to storageCache.
+//We need to clear localStorage *and* GM_storage
+var gmValuesList = GM_listValues();
+gmValuesList.forEach(key => {
+    if(key.indexOf('MagicTagReview-') === 0) {
+        var mainKey = key.replace(/^MagicTagReview-/,'');
+        var value = GM_getValue(key);
+        store[mainKey] = value;
+        GM_deleteValue(key);
+    }
 });
 
 /*Get values from in-page window properties (e.g. window.StackExchange...)
