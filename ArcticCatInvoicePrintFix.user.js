a3f92d8dad3c457be143feac51becd7d13ebc8e9
Tiny Giant <ajsa.ferguson@gmail.com>
2020-01-10 10:27:37 -0800

    Update ArcticCatInvoicePrintFix.user.js

--- ArcticCatInvoicePrintFix.user.js
+++ ArcticCatInvoicePrintFix.user.js
@@ -1,23 +1,12 @@
 // ==UserScript==
 // @name         Arctic Cat Invoice Print Fix
 // @namespace    http://github.com/Tiny-Giant
-// @version      1.0.0.0
+// @version      1.0.0.1
 // @description  Fixes Arctic Cat invoice printing
 // @author       @TinyGiant
 // @match        https://cattrackeronline.arcticcatinc.com/DP_Old_NWD/app/displayInvoice.do*
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
-var mywindow = window.open('', 'PRINT', 'height=900,width=900');
-
-mywindow.document.write('<html><head>');
-mywindow.document.write(document.head.innerHTML)
-mywindow.document.write('</head><body >');
-mywindow.document.write(document.body.innerHTML);
-mywindow.document.write('</body></html>');
-
-mywindow.focus(); // necessary for IE >= 10*/
-mywindow.print();
-mywindow.close();
-
-return true;
+window.print()
